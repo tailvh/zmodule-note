5363ff8ef852e5e42c69170bcf765e81c01aa68c
Le Tai <tailv@gmail.com>
2021-04-17 17:33:48 +0700

    20210417:173348

--- app/zmodel.js
+++ app/zmodel.js
@@ -1,8 +1,8 @@
 const ko = require('nekodb')
 
 let PreProps = {
-  name: ko.String[50].optional(),
-  desc: ko.String[50].optional(),
+  name: ko.String.default(''),
+  desc: ko.String.default(''),
   is_actived: ko.Number.default(1),
   is_deleted: ko.Number.default(0),
   created_at: ko.Number.now(),
